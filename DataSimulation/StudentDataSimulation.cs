fc7fe4f84628036f99ef6e80571c7ebe7f74d848
Ahmed Ali <A7medwhman@gmail.com>
2026-04-30 14:46:30 +0200

    server side -get all student

--- DataSimulation/StudentDataSimulation.cs
+++ DataSimulation/StudentDataSimulation.cs
@@ -2,5 +2,13 @@
 {
     public class StudentDataSimulation
     {
+        public static readonly List<Student> StudentList = new List<Student>
+        {
+            new Student{Id=1, Name = "ALI", Age =22 , Grade= 99},
+            new Student{Id=1, Name = "Ahmed", Age =34 , Grade= 78},
+            new Student{Id=1, Name = "omer", Age =88 , Grade= 67}
+,           new Student{Id=1, Name = "khaled", Age =12 , Grade= 50}
+
+        };
     }
 }
---
+++ StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using StudentAPI.DataSimulation;
+
+namespace StudentAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            return Ok (StudentDataSimulation.StudentList);
+        }
+    }
+}
