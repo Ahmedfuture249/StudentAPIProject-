51c28d49cac6e7811435a80587d28b9c837fd309
Ahmed Ali <A7medwhman@gmail.com>
2026-05-01 01:14:45 +0200

    update + remove

--- StudentController.cs
+++ StudentController.cs
@@ -72,5 +72,44 @@ namespace StudentAPI
             StudentDataSimulation.StudentList.Add(newStudent);
             return CreatedAtRoute("GetStudentByID", new { id = newStudent.Id }, newStudent);
         }
+
+        [HttpDelete("{id}", Name = "DeleteStudent")]
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult DeleteStudent(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Not eccepted ID {id}");
+            }
+            var student = StudentDataSimulation.StudentList.FirstOrDefault(s => s.Id == id);
+            if(student ==null)
+            {
+                return NotFound($"student with id {id} not found.");
+
+            }
+            StudentDataSimulation.StudentList.Remove(student);
+            return Ok($"Student with ID {id} has been removed ");
+        }
+        [HttpPut("{id}", Name = "EditStudent")]
+        public ActionResult EditStudent(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Not accepted ID {id}");
+            }
+            var student = StudentDataSimulation.StudentList.FirstOrDefault(s => s.Id == id);
+            if (student == null)
+            {
+                return NotFound($"student with id {id} not found.");
+            }
+            student.Name = "omer";
+            student.Age = 25;
+            student.Grade = 85;
+            return Ok(student);
+        }
     }
 }
